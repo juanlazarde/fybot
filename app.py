c3e270e7a20767c26bccef5fe9dfe889add4eac1
juanlazarde <64171337+juanlazarde@users.noreply.github.com>
2021-02-08 12:21:11 -0500

    Now with form input

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ via Flask,
 
 To run flask, open the python terminal and run: flask run"""
 
-import csv
 import datetime
 import json
 import os
@@ -15,19 +14,12 @@ import plotly as pt
 import plotly.graph_objs as go
 import talib
 import yfinance as yf
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 
 from patterns import candlestick_patterns
 
-# default settings, superseded from html form
-settings = {'consolidating': {'go': False, 'pct': 5},
-            'breakout': {'go': False, 'pct': 5},
-            'ttm_squeeze': {'go': False},
-            'candlestick': {'go': True},
-            'sma_filter': {'go': True, 'fast': 25, 'slow': 50}}
 
-
-class Const:
+class S:
     # define files
     dataset_dir = 'datasets'
     symbols_file = 'symbols.csv'
@@ -39,43 +31,39 @@ class Const:
     signals_file = os.path.join(dataset_dir, signals_file)
 
     # other constants
-    download = 100
+    download = 80
 
 
 class Filter:
-    def __init__(self):
+    def __init__(self, active_filter):
+        self.settings = active_filter
         # Read data and optimize
-        df = pd.read_pickle(Const.data_file)
-        remove_cols = ['Volume', 'Adj Close']
-        df = df.drop(remove_cols, axis=1, level=1)
+        df = pd.read_pickle(S.data_file)
+        df = df.drop(['Volume', 'Adj Close'], axis=1, level=1)
 
         # Extract symbols in data
         self.symbols = list(df.columns.levels[0])
         self.symbols.sort()
 
         # Define filters to use and create table with signals
-        columns = [k for k in settings if settings[k]['go']]
+        columns = [k for k in self.settings if self.settings[k]['go']]
         self.signal = pd.DataFrame(index=self.symbols, columns=columns)
 
         # Fill out signal table with different filters
-        i = 0
         for symbol in self.symbols:
-            # try:
             for column in columns:
                 self.signal.loc[symbol, column] = \
                     getattr(self, column)(df=df[symbol], symbol=symbol)
             # Remove latest symbol to optimize for speed
             df = df.drop(symbol, axis=1, level=0)
-            # except Exception as e:
-            #    i += 1
-            #    print('{} errors. {} symbol. {}'.format(i, symbol, e))
 
         # Save the signal table
-        self.signal.to_pickle(Const.signals_file)
+        self.signal.to_pickle(S.signals_file)
 
-    @staticmethod
-    def consolidating(df, pct=0, symbol='THE_DUMMY'):
-        pct = pct if pct > 0 else settings['consolidating']['pct']
+    def consolidating(self, **kwargs):
+        df = kwargs['df']
+        pct = kwargs['pct']
+        pct = pct if pct > 0 else self.settings['consolidating']['pct']
         recent_candlesticks = df[-15:]
         max_close = recent_candlesticks['Close'].max()
         min_close = recent_candlesticks['Close'].min()
@@ -84,26 +72,26 @@ class Filter:
             return True
         return False
 
-    def breakout(self, df, symbol='THE_DUMMY'):
-        pct = settings['breakout']['pct']
+    def breakout(self, **kwargs):
+        df = kwargs['df']
+        pct = self.settings['breakout']['pct']
         last_close = df[-1:]['Close'].values[0]
-        if self.consolidating(df[:-1], pct):
+        if self.consolidating(df=df[:-1], pct=pct):
             recent_closes = df[-16:-1]
             if last_close > recent_closes['Close'].max():
                 return True
         return False
 
     @staticmethod
-    def ttm_squeeze(df, symbol='THE_DUMMY'):
+    def ttm_squeeze(**kwargs):
+        df = kwargs['df']
         sq = pd.DataFrame()
         sq['20sma'] = df['Close'].rolling(window=20).mean()
         sq['stddev'] = df['Close'].rolling(window=20).std()
         sq['lower_band'] = sq['20sma'] - (2 * sq['stddev'])
         sq['upper_band'] = sq['20sma'] + (2 * sq['stddev'])
-
         sq['TR'] = abs(df['High'] - df['Low'])
         sq['ATR'] = sq['TR'].rolling(window=20).mean()
-
         sq['lower_keltner'] = sq['20sma'] - (sq['ATR'] * 1.5)
         sq['upper_keltner'] = sq['20sma'] + (sq['ATR'] * 1.5)
 
@@ -118,14 +106,16 @@ class Filter:
             return True
         return False
 
-    def candlestick(self, df, symbol):
+    def candlestick(self, **kwargs):
         """Based on hackthemarket"""
+        df = kwargs['df']
+        symbol = kwargs['symbol']
         bul, ber = [], []
 
         for pattern, description in candlestick_patterns.items():
             pattern_function = getattr(talib, pattern)
             results = pattern_function(
-                df["Open"], df["High"], df["Low"], df["Close"])
+                df['Open'], df['High'], df['Low'], df['Close'])
             last = int(results.tail(1).values[0])
             if last > 0:
                 bul.append(description)
@@ -142,16 +132,14 @@ class Filter:
             status = True
         return status
 
-    @staticmethod
-    def sma_filter(df, symbol='THE_DUMMY'):
+    def sma_filter(self, **kwargs):
+        df = kwargs['df']
         sma_fast = talib.SMA(df['Close'],
-                             timeperiod=settings['sma_filter']['fast'])
+                             timeperiod=self.settings['sma_filter']['fast'])
         sma_slow = talib.SMA(df['Close'],
-                             timeperiod=settings['sma_filter']['slow'])
+                             timeperiod=self.settings['sma_filter']['slow'])
 
-        if sma_fast[-1] > sma_slow[-1]:
-            return True
-        return False
+        return True if sma_fast[-1] > sma_slow[-1] else False
 
 
 class Chart:
@@ -203,31 +191,26 @@ class Snapshot:
         """Save table with symbol financial data from Yahoo Finance"""
 
         # create dataset directory if not there
-        if not os.path.exists(Const.dataset_dir):
-            os.makedirs(Const.dataset_dir)
+        if not os.path.exists(S.dataset_dir):
+            os.makedirs(S.dataset_dir)
 
-        # load symbols, if not there download s&p500 from Wikipedia
-        if os.path.isfile(Const.symbols_file):
-            df = pd.read_csv(Const.symbols_file)
+        # load symbols, if not there download s&p500
+        if os.path.isfile(S.symbols_file):
+            df = pd.read_csv(S.symbols_file)
         else:
-            df = self.download_sp500(Const.symbols_file)
+            df = self.download_sp500(S.symbols_file)
 
         # download and save financial data for each symbol
         symbols = list(df['Symbol'])
-        # replace '.' with '-' a Yahoo Finance issue
-        symbols = [x.replace('.', '-') for x in symbols]
         end_date = datetime.datetime.now()
-        start_date = end_date - datetime.timedelta(days=Const.download)
+        start_date = end_date - datetime.timedelta(days=S.download)
         data = yf.download(symbols,
                            start=start_date,
                            end=end_date,
                            group_by="ticker")
         data = data.dropna(how='all')
         data = data.dropna(how='all', axis=1)
-        data.to_pickle(Const.data_file)
-
-        # Apply filters to data once and save in separate file
-        Filter()
+        data.to_pickle(S.data_file)
 
     @staticmethod
     def download_sp500(symbols_file):
@@ -236,6 +219,9 @@ class Snapshot:
             'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
         df = table[0]
         df = df[['Symbol', 'Security', 'GICS Sector', 'GICS Sub-Industry']]
+        # replace '.' with '-' a Yahoo Finance issue
+        df = df.assign(
+            Symbol=lambda x: x['Symbol'].str.replace('.', '-', regex=True))
         df.to_csv(symbols_file, index=False)
         return df
 
@@ -244,31 +230,27 @@ class Index:
     signals = None
     stocks = None
 
-    def __init__(self):
+    def __init__(self, active_filter):
+        self.settings = active_filter
         self.load_data()
         self.apply_filters()
         self.prep_arguments()
 
     def load_data(self):
         # load company names and signal table
-        self.signals = pd.read_pickle(Const.signals_file)
+        self.signals = pd.read_pickle(S.signals_file)
 
-        df = pd.read_csv(Const.symbols_file, index_col='Symbol',
-                         usecols=[0, 1])
+        df = pd.read_csv(S.symbols_file, index_col='Symbol', usecols=[0, 1])
         self.stocks = df.T.to_dict()
-
-        # with open(Const.symbols_file) as f:
-        #     self.stocks = {row[0]: {"company": row[1]}
-        #                    for row in csv.reader(f)}
 
     def apply_filters(self):
         """Filter signal table with selections"""
         signals = self.signals
 
         # only show the filters wanted
-        for k in settings.keys():
+        for k in self.settings.keys():
             if k in signals.columns:
-                signals = signals[signals[k] == settings[k]['go']]
+                signals = signals[signals[k] == self.settings[k]['go']]
 
         # remove the nan
         signals = signals.fillna('')
@@ -285,7 +267,7 @@ class Index:
                   if k in list(self.signals.index)}
 
         # send candle info to server
-        if settings['candlestick']['go']:
+        if self.settings['candlestick']['go']:
             for symbol, row in self.signals.iterrows():
                 stocks[symbol]['cdl_sum_ber'] = row.cdl_sum_ber
                 stocks[symbol]['cdl_sum_bul'] = row.cdl_sum_bul
@@ -300,21 +282,53 @@ app = Flask(__name__)
 @app.route("/snapshot")
 def snapshot_wrapper():
     Snapshot()
-    return "Success downloading data." \
-           " <a href='http://localhost:5000'>Go Back</a>"
+    return "Success downloading data. " \
+           "<a href='http://localhost:5000'>Go Back</a>"
 
 
-@app.route("/filter")
-def filter_wrapper():
-    Filter()
-    return "Success filtering. <a href='http://localhost:5000'>Go Back</a>"
-
-
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    stocks = Index().stocks
+    stocks = {}
+
+    if request.method == 'GET':
+        if os.path.isfile(S.data_file):
+            modified_time = os.stat(S.data_file).st_mtime
+            last_modified = datetime.datetime.fromtimestamp(modified_time)
+            now = datetime.datetime.now()
+            last24 = datetime.timedelta(hours=24)
+            if now - last_modified > last24:
+                Snapshot()
+
+    if request.method == 'POST':
+        # read arguments from form
+        filters_selected = request.form.getlist('filter')
+        if len(filters_selected) == 0:
+            return "Please go back and make a selection. " \
+                   "<a href='http://localhost:5000'>Go Back</a>"
+
+        # shorten functions
+        def fltr_me(x): return x in filters_selected
+        frm_get = request.form.get
+
+        active_filters = {'consolidating':
+                          {'go': fltr_me('consolidating'),
+                           'pct': frm_get('consolidating_pct')},
+                          'breakout':
+                          {'go': fltr_me('breakout'),
+                           'pct': frm_get('breakout_pct')},
+                          'ttm_squeeze':
+                          {'go': fltr_me('ttm_squeeze')},
+                          'candlestick':
+                          {'go': fltr_me('candlestick')},
+                          'sma_filter':
+                          {'go': fltr_me('sma_filter'),
+                           'fast': frm_get('sma_fast'),
+                           'slow': frm_get('sma_slow')}}
+
+        Filter(active_filters)
+        stocks = Index(active_filters).stocks
+
     return render_template("index.html",
-                           candlestick_patterns=candlestick_patterns,
                            stocks=stocks)
 
 
@@ -383,7 +397,6 @@ if __name__ == "__main__":
 #  )
 
 #
-# # TODO: Shorten these up
 # df = pd.read_csv(Const.symbols_file)
 # df = df[['Symbol', 'Security']]
 # df = df.T
@@ -394,6 +407,14 @@ if __name__ == "__main__":
 # #        # replace '.' with '-' a Yahoo Finance issue
 #         df = df.assign(
 #             Symbol=lambda x: x['Symbol'].str.replace('.', '-', regex=True))
+
+# with open(Const.symbols_file) as f:
+#     self.stocks = {row[0]: {"company": row[1]}
+#                    for row in csv.reader(f)}
+
+# # replace '.' with '-' a Yahoo Finance issue
+# symbols = [x.replace('.', '-') for x in symbols]
+
 
 # Tables to html
 # https://stackoverflow.com/questions/52644035/how-to-show-a-pandas-dataframe-into-a-existing-flask-html-table
