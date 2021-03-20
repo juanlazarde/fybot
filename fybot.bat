8c381fa7d98c43b50bf1df71c3282f32fdbc5048
juanlazarde <64171337+juanlazarde@users.noreply.github.com>
2021-03-20 00:45:22 -0400

    clearing up

--- fybot.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo I'll give you stocks
-call "C:\Users\juanlazarde\Documents\Python Scripts\candlestick screener\venv\Scripts\activate.bat"
-"C:\Users\juanlazarde\Documents\Python Scripts\candlestick screener\venv\Scripts\python.exe" "C:\Users\juanlazarde\Documents\Python Scripts\candlestick screener\app.py"
-call "C:\Users\juanlazarde\Documents\Python Scripts\candlestick screener\venv\Scripts\deactivate.bat"
-pause
-exit
